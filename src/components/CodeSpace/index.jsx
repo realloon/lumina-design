fbecb478c8851c68f85b087995c0638f2c9ae543
sincerajn <sincerajn@outlook.com>
2023-08-02 14:30:19 +0800

    ~

--- src/components/CodeSpace/index.jsx
+++ src/components/CodeSpace/index.jsx
@@ -4,9 +4,6 @@ const CodeSpace = ({ title, children }) => {
   return (
     <section className="lumina-code-space">
       <h2 id={title} className="lumina-code-space-title">
-        <a href={'#' + title} className="lumina-code-space-link">
-          #
-        </a>
         {title}
       </h2>
 
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,31 +1,11 @@
-import { useRef } from 'react'
 import './index.css'
 
-const Input = ({ type = 'text', placeholder, ...rest }) => {
-  const input = useRef(null)
-
-  return (
-    <div className="lumina-input-wrapper">
-      <span
-        className="lumina-input-placeholder"
-        onClick={() => input.current.focus()}
-        onMouseOver={() => input.current.focus()}
-      >
-        {placeholder}
-      </span>
-      <input
-        ref={input}
-        className="lumina-input"
-        type={type}
-        placeholder=" "
-        {...rest}
-      />
-
-      <label className="lumina-switch">
-        <input type="checkbox" defaultChecked={true} />
-      </label>
-    </div>
-  )
+const Input = ({ type = 'text', ...rest }) => {
+  return <input className={`lumina-input lumina-input-${type}`} {...rest} />
 }
 
 export default Input
+
+/* <label className="lumina-switch">
+  <input type="checkbox" defaultChecked={true} />
+</label> */
